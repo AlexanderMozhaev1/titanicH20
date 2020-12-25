d22addde7bb9b43ac97a928d19ee2d48b65c83e4
alexander <alexandermozhaev1@gmail.com>
2020-12-25 20:15:18 +0300

    Part 3

--- titanicH20/notebooks/t.py
+++
@@ -1,3 +0,0 @@
-from titanicH20.src.data.make_dataset import load_data
-
-load_data()
--- titanicH20/src/models/train_model.py
+++ titanicH20/src/models/train_model.py
@@ -1,7 +1,9 @@
 from h2o.automl import H2OAutoML
 
 
-def train(y, df):
-    aml = H2OAutoML(max_models=100, max_runtime_secs=600, seed=22)
+def train(y, df, params):
+    aml = H2OAutoML(max_models=params['max_models'],
+                    max_runtime_secs=params['max_runtime_secs'],
+                    seed=params['seed'])
     aml.train(y=y, training_frame=df)
     return aml
